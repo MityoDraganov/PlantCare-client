afddf2403f102fdcc6831ab358033200d170c768
Mityo Draganov <mityodraganow@gmail.com>
2024-07-23 01:05:12 +0300

    init add crop pot page

--- src/Pages/AddCropPot/AddCropPot.tsx
+++ src/Pages/AddCropPot/AddCropPot.tsx
@@ -1,7 +1,43 @@
+import { SignedIn, SignedOut, SignUp, useUser } from "@clerk/clerk-react";
+import { Card } from "../../components/ui/card";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { Button } from "../../components/ui/button";
+import { ChevronRight } from "lucide-react";
+
 export const AddCropPot = () => {
-    return(
-        <div>
-            <h2>Mityo Draganov</h2>
+  const { user } = useUser();
+
+  return (
+    <div className="flex flex-col gap-[5%] h-full py-[5%]  items-center">
+      <div>
+        <h1 className="text-3xl font-mono">Plant Care</h1>
+        <p>Add Crop-Pot</p>
+      </div>
+      <SignedOut>
+        <SignUp forceRedirectUrl={"void:0"} />
+      </SignedOut>
+      <SignedIn>
+        <div className="w-[80%] h-full justify-between items-center flex flex-col">
+          <div className="w-full h-fit flex flex-col">
+            <p className="p-0 text-left font-semibold pl-2">Signed in as:</p>
+            <Card className="w-full h-fit p-[2%] flex gap-[5%]">
+              <Avatar asChild className="sm:w-[20%] md:w-[10%] rounded-full">
+                <AvatarImage src={user?.imageUrl} />
+              </Avatar>
+              <div className="flex flex-col text-left">
+                <h2>
+                  {user?.firstName} {user?.lastName}
+                </h2>
+                <p>{user?.emailAddresses?.[0]?.emailAddress}</p>
+              </div>
+            </Card>
+          </div>
+
+          <Button className="flex items-center justify-between px-[5%] h-[10%] w-2/3">
+            Continue <ChevronRight />
+          </Button>
         </div>
-    )
-}
+      </SignedIn>
+    </div>
+  );
+};
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,16 @@
 import { SignIn } from "@clerk/clerk-react"
+import { Button } from "../../components/ui/button"
 
 export const HomePage = () => {
     return(
-        <div className="flex justify-center">
+        <div className="flex flex-col justify-center">
             <h2 className="text-3xl font-mono">Plant Care</h2>
             
+            <div>
+                
+            </div>
+            <Button>Get started</Button>
             
         </div>
     )
-}
+} 
