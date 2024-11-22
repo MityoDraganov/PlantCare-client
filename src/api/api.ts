4c138ebcfeb531df0046a7bda68ed621d11fcf9a
Mityo Draganov <mityodraganow@gmail.com>
2024-11-22 11:15:56 +0200

    build

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ import { Clerk } from "@clerk/clerk-js";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-const host = "https://plantscare.sytes.net/api/v1/cropPots";
+const host = "https://plantscare.sytes.net/api/v1/";
 
 interface RequestOptions {
 	method: string;
